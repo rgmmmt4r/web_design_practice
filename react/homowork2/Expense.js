0bc56412dec34fbbf3aa9acb77f8bf03f0937c7c
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2022-05-05 16:22:56 +0800

    homework2

---
+++ react/homowork2/Expense.js
@@ -0,0 +1,42 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from '../UI/Card'
+import './Expenses.css'
+import ExpensesFilter from "./ExpenseFilter";
+
+
+function Expense(props) {
+    const selectYearHandler = (year)=>{
+        console.log("In Expense.js")
+        console.log(year)
+    }
+
+    return (
+        <ExpensesFilter onSelectYear = {selectYearHandler}>
+            <Card className="expenses">
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}>
+                </ExpenseItem>
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}>
+                </ExpenseItem>
+            </Card>
+        </ExpensesFilter>
+
+    )
+}
+
+export default Expense;
